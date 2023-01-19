eefe5152a8309dcf95e7ce3f7eeeeb98d50c5bdd
redi <redi.vreto@studenti.unitn.it>
2023-01-19 18:27:06 +0100

    set up pddl domain, belief and desire set, launcher and action nodes

---
+++ bdi_spazza/src/actions/pickup.cpp
@@ -0,0 +1,29 @@
+#include "ros2_bdi_skills/bdi_action_executor.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+class Pickup : public BDIActionExecutor
+{
+    public:
+        Pickup()
+        : BDIActionExecutor("pickup", 4)
+        {}
+
+        float advanceWork()
+        {
+            return 0.0625f;            
+        }
+
+    private:
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  
+  auto actionNode = std::make_shared<Pickup>();
+  rclcpp::spin(actionNode->get_node_base_interface());
+
+  rclcpp::shutdown();
+
+  return 0;
+}
