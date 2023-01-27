68be3cb5cee9b052c5be8a612d1181ff1f80cf76
redi <redi.vreto@studenti.unitn.it>
2023-01-27 12:30:34 +0100

    remove unused directories

--- build/collision_avoidance/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/redi/examples/install/collision_avoidance'
--- build/litter/build/lib/litter/robot_driver.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from litter.')
-
-
-if __name__ == '__main__':
-    main()
